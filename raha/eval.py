03817e43c51a434008d9a97c928d0333b9ab1590
Yuchuan Huang <itisyuchuan@gmail.com>
2024-03-05 21:12:31 -0800

    add sparcle experiment driver and evaluation

---
+++ raha/eval.py
@@ -0,0 +1,86 @@
+import duckdb
+import pandas as pd
+
+
+def do_eval(data, eval_attrs, output_file):
+    df_raw = data.dataframe
+    df_clean = data.clean_dataframe
+    df_repair = data.repaired_dataframe
+
+    column_rename = ", ".join([f"t1.{attr} as {attr}, t2.{attr} as {attr}_gt, t3.{attr} as {attr}_repair" for attr in eval_attrs])
+
+    sql = f'select t1.id, t1.x, t1.y, {column_rename} from df_raw t1, df_clean t2, df_repair t3 where t1.id = t2.id and t1.id = t3.id'
+    df_eval = duckdb.query(sql).to_df()
+
+    null_count = 0
+    wrong_count = 0
+    error_count = 0
+    repair_count = 0
+    correct_repair_count = 0
+    eval_stat = []
+
+    for attr in eval_attrs:
+        sql_null = f"SELECT count(1) FROM df_eval WHERE {attr} is null"
+        sql_wrong = f"SELECT count(1) FROM df_eval WHERE {attr} is not null AND {attr} <> {attr}_gt"
+        attr_null_count = duckdb.query(sql_null).fetchone()[0]
+        attr_wrong_count = duckdb.query(sql_wrong).fetchone()[0]
+
+        sql_error = f"SELECT count(1) FROM df_eval WHERE {attr} <> {attr}_gt"
+        sql_repair = f"SELECT count(1) FROM df_eval WHERE {attr} <> {attr}_repair"
+        sql_correct_repair = f"SELECT count(1) FROM df_eval WHERE {attr} <> {attr}_gt AND {attr}_repair = {attr}_gt"
+        attr_error_count = duckdb.query(sql_error).fetchone()[0]
+        attr_repair_count = duckdb.query(sql_repair).fetchone()[0]
+        attr_correct_repair_count = duckdb.query(sql_correct_repair).fetchone()[0]
+
+        null_count += attr_null_count
+        wrong_count += attr_wrong_count
+        error_count += attr_error_count
+        repair_count += attr_repair_count
+        correct_repair_count += attr_correct_repair_count
+
+        if attr_repair_count == 0:
+            precision = -1
+        else:
+            precision = attr_correct_repair_count / attr_repair_count
+        recall = attr_correct_repair_count / attr_error_count
+
+        attr_stat = {
+            "attr": attr,
+            "null_count": attr_null_count,
+            "wrong_count": attr_wrong_count,
+            "error_count": attr_error_count,
+            "repair_count": attr_repair_count,
+            "correct_repair_count": attr_correct_repair_count,
+            "precision": precision,
+            "recall": recall,
+            "f1": 2 * precision * recall / (precision + recall),
+        }
+        eval_stat.append(attr_stat)
+
+    if repair_count == 0:
+        precision = -1
+    else:
+        precision = correct_repair_count / repair_count
+    recall = correct_repair_count / error_count
+    overall_stat = {
+        "attr": "overall",
+        "null_count": null_count,
+        "wrong_count": wrong_count,
+        "error_count": error_count,
+        "repair_count": repair_count,
+        "correct_repair_count": correct_repair_count,
+        "precision": precision,
+        "recall": recall,
+        "f1": 2 * precision * recall / (precision + recall),
+    }
+    eval_stat.append(overall_stat)
+
+    df_eval_stat = pd.DataFrame(eval_stat)
+    df_eval_stat = df_eval_stat[["attr", "precision", "recall", "f1", "null_count", "wrong_count", "error_count", "repair_count", "correct_repair_count"]]
+    t = df_eval_stat.set_index("attr").T
+    t = t.rename_axis("metric", axis=1)
+    t = t[["overall"] + eval_attrs]
+    pd.set_option("display.precision", 3)
+    print(t)
+
+    t.to_csv(output_file, float_format="%.3f")
---
+++ raha/sparcle_exp_driver.py
@@ -0,0 +1,53 @@
+import argparse
+import time
+import os
+import raha
+from raha import Detection, Correction
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from eval import do_eval
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--Dataset")
+    parser.add_argument("-t", "--Times")
+    args = parser.parse_args()
+    dataset_name = args.Dataset
+    times = int(args.Times)
+    print(f"Dataset: {dataset_name}, Times: {times}")
+
+    # Load the dataset
+    dataset_dictionary = {
+        "name": dataset_name,
+        "path": os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "datasets", dataset_name, "dirty.csv")),
+        "clean_path": os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "datasets", dataset_name, "clean.csv"))
+    }
+    data = raha.dataset.Dataset(dataset_dictionary)
+
+    for counter in range(times):
+        # error detection
+        app_detect = Detection()
+        app_detect.VERBOSE = False
+        app_detect.SAVE_RESULTS = False
+        detection_dictionary = app_detect.run(dataset_dictionary)
+        p, r, f = data.get_data_cleaning_evaluation(detection_dictionary)[:3]
+        print("Raha's performance on {}:\nPrecision = {:.2f}\nRecall = {:.2f}\nF1 = {:.2f}".format(data.name, p, r, f))
+
+        # error correction
+        data.detected_cells = detection_dictionary
+        app_correct = Correction()
+        app_correct.VERBOSE = True
+        app_correct.SAVE_RESULTS = False
+        correction_dictionary = app_correct.run(data)
+        p, r, f = data.get_data_cleaning_evaluation(correction_dictionary)[-3:]
+        print("Baran's performance on {}:\nPrecision = {:.2f}\nRecall = {:.2f}\nF1 = {:.2f}".format(data.name, p, r, f))
+        data.create_repaired_dataset(correction_dictionary=correction_dictionary)
+
+        # attribute evaluation
+        end = time.perf_counter()
+        runtime = time.strftime("%Hh%Mm%Ss", time.gmtime(end - start))
+        output_file = f'{dataset_dictionary["name"]}_eval_{counter}_{runtime}.csv'
+        do_eval(data, dataset_dictionary["eval_attrs"], output_file)
